cc24ee1911cf1e339d274e1ec602dbbc1a5a5987
Singh Shubham <Shubham563480@gmail.com>
2026-04-20 10:31:57 +0000

    Fixed syntax error in app.py

--- app.py
+++ app.py
@@ -7,13 +7,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "DevOps Project Running"
-
-if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
