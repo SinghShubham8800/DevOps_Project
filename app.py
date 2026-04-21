0c411f923f68447ced245ea89b44552dc1e5edd5
Singh Shubham <Shubham563480@gmail.com>
2026-04-21 12:29:21 +0000

    Integrated PostgreSQL and Nginx into the architecture

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+import os
+import psycopg2
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+def get_db_connection():
+    # 'db' is the hostname defined in docker-compose
+    conn = psycopg2.connect(host='db',
+                            database='myapp',
+                            user='user',
+                            password='password')
+    return conn
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT version();')
+    db_version = cur.fetchone()
+    cur.close()
+    conn.close()
+    return render_template('index.html', db_version=db_version)
